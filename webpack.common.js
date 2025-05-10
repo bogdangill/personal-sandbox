d77040dc51a42c656445fcfb8170dc053a047c3d
Bogdan <herecomesthefog@gmail.com>
2025-05-10 11:59:03 +0500

    add loaders for html & markdown

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,11 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
-            }
+            },
+            {
+                test: /\.md$/,
+                use: ['html-loader', 'markdown-loader'],
+            },
         ]
     },
 }
