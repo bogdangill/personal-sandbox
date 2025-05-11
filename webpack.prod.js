f3213deb71811c2195ddf912b33a5e2e93132de4
Bogdan <herecomesthefog@gmail.com>
2025-05-11 08:31:49 +0500

    add css extract config for build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const common = require('./webpack.common');
 const {merge} = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -9,5 +10,21 @@ module.exports = merge(common, {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'build'),
         clean: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, //2 извлекает цсс в файл
+                    'css-loader' //1 превращает цсс в cjs
+                ]
+            },
+        ]
     },
 })
