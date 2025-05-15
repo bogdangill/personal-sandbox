9692655fafc3d46d352d3dee1fd8a938094fc082
Bogdan <herecomesthefog@gmail.com>
2025-05-15 21:23:23 +0500

    add theme switcher (demo unfinished buggy)

--- src/app.js
+++ src/app.js
@@ -12,9 +12,55 @@ hljs.registerLanguage('javascript', javascript);
 //веб-компоненты Шнурков
 import '@shoelace-style/shoelace/dist/components/divider/divider.js';
 import '@shoelace-style/shoelace/dist/components/button/button';
+import '@shoelace-style/shoelace/dist/components/icon/icon';
 
 const taskDescription = document.getElementById('task-description');
 const taskSolution = document.getElementById('task-solution');
+const root = document.querySelector('html');
+
+function checkUserThemePreference() {
+    if (window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches) {
+        return 'dark';
+    } else {
+        return 'light';
+    }
+}
+
+function switchTheme() {
+    if (root.classList.contains(`sl-theme-dark`)) {
+        root.classList.remove(`sl-theme-dark`)
+        root.classList.add(`sl-theme-light`);
+    } else {
+        root.classList.remove(`sl-theme-light`)
+        root.classList.add(`sl-theme-dark`);
+    }
+}
+
+function renderThemeSwitcher(theme = 'light') {
+    const themeSwitcherContainer = document.getElementById('theme-switcher');
+    const themeSwitcherIcon = Object.assign(document.createElement('sl-icon'), {
+        name: theme === 'light' ? 'cloud-moon' : 'cloud-sun',
+        label: 'Сменить тему'
+    });
+    const themeSwitcherButton = Object.assign(document.createElement('sl-button'),
+        {
+            circle: true,
+            size: 'medium'
+        }
+    );
+    themeSwitcherContainer.append(themeSwitcherButton);
+    themeSwitcherButton.append(themeSwitcherIcon);
+    
+    themeSwitcherButton.addEventListener('click', () => {
+        switchTheme()
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const theme = checkUserThemePreference();
+    root.classList.add(`sl-theme-${theme}`);
+    renderThemeSwitcher(theme);
+})
 
 async function loadTask(taskName) {
     try {
