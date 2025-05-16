25ad87d0db56b3bae3b4eddf870ea8d9b06a3316
Bogdan <herecomesthefog@gmail.com>
2025-05-17 00:48:13 +0500

    add hint for switcher

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,5 +1,6 @@
 import '@shoelace-style/shoelace/dist/components/button/button';
 import '@shoelace-style/shoelace/dist/components/icon/icon';
+import '@shoelace-style/shoelace/dist/components/tooltip/tooltip';
 
 const STORAGE_THEME_KEY = 'is-dark-theme';
 const DARK_THEME_CLASS = 'sl-theme-dark';
@@ -24,7 +25,14 @@ export function definePreferedTheme() {
 export function renderThemeSwitcher() {
     const themeSwitcherContainer = document.getElementById('theme-switcher');
     const switchIcon = () => localStorage.getItem(STORAGE_THEME_KEY) ? 'cloud-sun' : 'cloud-moon';
+    const switchHint = () => localStorage.getItem(STORAGE_THEME_KEY) ? 'Включить светлую тему' : 'Включить темную тему';
 
+    const themeSwitcherTooltip = Object.assign(document.createElement('sl-tooltip'),
+        {
+            content: switchHint(),
+            placement: 'right'
+        }
+    );
     const themeSwitcherIcon = Object.assign(document.createElement('sl-icon'), 
         {
             name: switchIcon(),
@@ -38,11 +46,13 @@ export function renderThemeSwitcher() {
         }
     );
 
-    themeSwitcherContainer.append(themeSwitcherButton);
+    themeSwitcherContainer.append(themeSwitcherTooltip);
+    themeSwitcherTooltip.append(themeSwitcherButton);
     themeSwitcherButton.append(themeSwitcherIcon);
     
     themeSwitcherButton.addEventListener('click', () => {
         switchDarkTheme();
         themeSwitcherIcon.name = switchIcon();
+        themeSwitcherTooltip.content = switchHint();
     })
 }
