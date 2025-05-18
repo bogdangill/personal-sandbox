22f3c6bb4bb177bdba8f00025cc01dc6a6dce9c5
Bogdan <herecomesthefog@gmail.com>
2025-05-18 21:41:41 +0500

    create task interface & base task data structures

---
+++ src/js/tasks.js
@@ -0,0 +1,12 @@
+function TaskInterface(id, name, text, code, startDate, endDate = startDate, importance = null) {
+    this.id = id;
+    this.name = name;
+    this.text = text;
+    this.code = code;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.importance = importance;
+}
+
+const currentTask = {};
+const tasksData = [];
