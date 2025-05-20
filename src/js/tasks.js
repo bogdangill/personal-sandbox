08ba7b02e21e2f897185dffe7b266373435e0836
Bogdan <herecomesthefog@gmail.com>
2025-05-20 22:51:06 +0500

    refactoring pt.1 (unfinished)

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -13,58 +13,157 @@ function Task(id, name, text, code = null, startDate, endDate = startDate, impor
     this.endDate = endDate;
     this.importance = importance;
 }
+
 let currentTask = {};
 const tasksData = JSON.parse(localStorage.getItem('data')) || [];
 
-export function renderTaskDescriptionForm() {
-    const taskDescription = document.getElementById('task-description');
-    taskDescription.innerHTML = `
-        <form id="task-description-form">
-        </form>
-    `;
-    const taskDescriptionForm = document.getElementById('task-description-form');
-    
-    const taskNameInput = Object.assign(document.createElement('sl-input'), {
+//helpers
+function escapeHtml(html) {
+    const div = document.createElement('div');
+    div.textContent = html;
+    return div.innerHTML;
+}
+function notify(message, variant = 'warning', icon = 'exclamation-triangle', duration = 3000) {
+    const formAlert = Object.assign(document.createElement('sl-alert'), {
+        duration: duration,
+        closable: true,
+        countdown: 'rtl',
+        variant: variant,
+        innerHTML: `
+            <sl-icon name="${icon}" slot="icon"></sl-icon>
+            ${escapeHtml(message)}
+        `
+    });
+
+    document.body.append(formAlert);
+    return formAlert.toast();
+}
+
+//фабрика элементов формы из компонентов Shoelace
+function createTaskNameInput() {
+    return Object.assign(document.createElement('sl-input'), {
         label: 'Название задачи',
         placeholder: 'e.g. Bubble sort',
         clearable: true,
+        autocomplete: 'off'
     });
-    const taskTextarea = Object.assign(document.createElement('sl-textarea'), {
+}
+function createTaskTextarea() {
+    return Object.assign(document.createElement('sl-textarea'), {
         label: 'Описание задачи',
         resize: 'auto',
         helpText: 'p.s. описание можно оформить в формате markdown - тогда после сохранения оформленный текст будет выводиться обернутый html-тегами',
+        minlength: 10
     });
+}
+function createSubmitButton() {
     const submitButton = Object.assign(document.createElement('sl-button'), {
         variant: 'primary',
         type: 'submit',
         disabled: true
     });
+    submitButton.innerText = "Сохранить";
+
+    return submitButton;
+}
+function createResetButton() {
     const resetButton = Object.assign(document.createElement('sl-button'), {
         variant: 'neutral',
     });
-
-    submitButton.innerText = "Сохранить";
     resetButton.innerText = "Сбросить";
 
+    return resetButton;
+}
+function createFormButtons(...buttons) {
     const buttonWrapper = document.createElement('div');
     buttonWrapper.classList.add('form-buttons');
+    buttonWrapper.append(...buttons);
 
-    taskDescriptionForm.append(taskNameInput);
-    taskDescriptionForm.append(taskTextarea);
-    taskDescriptionForm.append(buttonWrapper);
-    buttonWrapper.append(submitButton);
-    buttonWrapper.append(resetButton);
+    return buttonWrapper;
+}
+function createTaskDescriptionForm() {
+    const taskDescriptionForm = document.createElement('form');
+    taskDescriptionForm.setAttribute('id', 'task-description-form');
 
-    taskTextarea.addEventListener('sl-input', () => {
-        submitButton.disabled = taskTextarea.value.length <= 0;
-    });
-    resetButton.addEventListener('click', clearFormInputs);
+    return taskDescriptionForm;
+}
 
-    function clearFormInputs() {
-        taskNameInput.value = '';
-        taskTextarea.value = '';
-        submitButton.disabled = true;
+const taskDescriptionForm = {
+    root: createTaskDescriptionForm(),
+    nameInput: createTaskNameInput(),
+    textarea: createTaskTextarea(),
+    submitButton: createSubmitButton(),
+    resetButton: createResetButton(),
+    
+    appendChildren() {
+        this.root.append(this.nameInput);
+        this.root.append(this.textarea);
+        this.root.append(createFormButtons(this.submitButton, this.resetButton));
     }
+}
+
+const FormManager = {
+    form: taskDescriptionForm,
+
+    clearInputs() {
+        this.form.nameInput.value = '';
+        this.form.textarea.value = '';
+    },
+    async disableSubmitButton() {
+        await Promise.all([
+            customElements.whenDefined('sl-textarea'),
+            customElements.whenDefined('sl-button')
+        ]).then(() => {
+            this.form.textarea.addEventListener('sl-input', () => {
+                this.form.submitButton.disabled = this.form.textarea.value.length <= 0;
+            })
+        });
+    },
+    async reset() {
+        await Promise.all([
+            customElements.whenDefined('sl-input'),
+            customElements.whenDefined('sl-textarea'),
+            customElements.whenDefined('sl-button')
+        ]).then(() => {
+            this.form.addEventListener('reset', () => {
+                this.clearInputs();
+                this.form.submitButton.disabled = true;
+            })
+        });
+    },
+    async validate() {
+        await Promise(customElements.whenDefined('sl-textarea'))
+            .then(() => {
+                this.form.addEventListener('sl-invalid', evt => {
+                    evt.preventDefault();
+                    notify(`Ошибка: ${evt.target.validationMessage}`);
+                    evt.target.focus();
+                },{ capture: true })
+            });
+    },
+    async submit() {
+        await Promise.all([
+            customElements.whenDefined('sl-input'),
+            customElements.whenDefined('sl-textarea'),
+            customElements.whenDefined('sl-button')
+        ]).then(() => {
+            this.form.addEventListener('submit', evt => {
+                evt.preventDefault();
+                //здесь должен быть метод из объекта TaskDataManager(или хз как его назвать пока) который будет добавлять данные задачи из формы в конструктор и отправлять в LS
+                this.clearInputs();
+                this.form.submitButton.disabled = true;
+                setTimeout(() => notify('Описание успешно сохранено!', 'success', 'check-square'), 500);
+            })
+        });
+    }
+}
+
+export function renderTaskDescriptionForm() {
+    const taskDescription = document.getElementById('task-description');
+    const form = taskDescriptionForm;
+    form.appendChildren();
+    taskDescription.append(form.root);
+
     function createTaskDescription(name, text) {
         const date = new Date();
         const id = Date.now();
@@ -86,26 +185,6 @@ export function renderTaskDescriptionForm() {
         } else {
             return true;
         }
-    }
-    function escapeHtml(html) {
-        const div = document.createElement('div');
-        div.textContent = html;
-        return div.innerHTML;
-    }
-    function notify(message, variant = 'warning', icon = 'exclamation-triangle', duration = 3000) {
-        const formAlert = Object.assign(document.createElement('sl-alert'), {
-            duration: duration,
-            closable: true,
-            countdown: 'rtl',
-            variant: variant,
-            innerHTML: `
-                <sl-icon name="${icon}" slot="icon"></sl-icon>
-                ${escapeHtml(message)}
-            `
-        });
-
-        document.body.append(formAlert);
-        return formAlert.toast();
     }
 
     taskDescriptionForm.addEventListener('submit', (evt) => {
