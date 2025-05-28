e327f03802a44a7c50324fc66cb5d5f6150af3e5
Bogdan <herecomesthefog@gmail.com>
2025-05-28 20:57:59 +0500

    add demo solutionForm logic

---
+++ src/js/solutionForm.js
@@ -0,0 +1,67 @@
+import { UIComponentFactory } from './UIComponentFactory';
+import { notify } from './helpers';
+import { storageEntities, storageManager } from './storageService';
+
+const taskSolutionFormView = {
+    root: UIComponentFactory.createTaskForm(),
+    textarea: UIComponentFactory.createTaskSolutionTextarea(),
+    executeButton: UIComponentFactory.createButton('warning', 'Выполнить'),
+    submitButton: UIComponentFactory.createButton('success', 'Сохранить', true),
+    _isMounted: false,
+
+    appendChildren() {
+        if (this._isMounted) return; //избегаю повторного монтирования
+        this.root.append(this.textarea);
+        this.root.append(UIComponentFactory.createFormButtons(this.executeButton, this.saveButton));
+        this._isMounted = true;
+    },
+    mount(containerSelector) {
+        const container = document.querySelector(containerSelector);
+        this.appendChildren();
+        container.append(this.root);
+    },
+    unmount() {
+        if (!this._isMounted) return;
+        this.root.remove();
+        this._isMounted = false;
+    }
+}
+
+export const taskSolutionFormController = {
+    form: taskSolutionFormView,
+    formElement: taskSolutionFormView.root,
+    /**
+     * Отключает кнопку сохранения, если поле с решением пустое.
+     */
+    disableSubmitButton() {
+        this.form.textarea.addEventListener('input', () => {
+            this.form.submitButton.disabled = this.form.textarea.value.trim().length <= 0;
+        })
+    },
+    execute() {
+        this.form.executeButton.addEventListener('click', () => {
+            //будет запускаться анализ кода решения и выводиться результат выполнения console.log или alert();
+            notify('Код выполнился, результат выполнения', 'neutral');
+        });
+    },
+    save() {
+        this.form.submitButton.addEventListener('click', () => {
+            const descriptionFormData = storageManager.get(storageEntities.DESCRIPTION_FORM_DATA);
+            const currentTaskData = createTaskData(descriptionFormData.title, descriptionFormData.description, this.form.textarea.value);
+
+            storageManager.set(storageEntities.CURRENT_TASK_DATA, currentTaskData);
+
+            setTimeout(() => notify('Задача сохранена и добавлена в коллекцию!', 'success', 'check-square'), 500);
+        })
+    },
+
+    async bindEvents() {
+        await Promise.all([
+            customElements.whenDefined('sl-button')
+        ]).then(() => {
+            this.disableSubmitButton();
+            this.save();
+            this.execute();
+        });
+    }
+}
