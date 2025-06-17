8800c2bae9c13ec5afb640945f0b7306a3af9962
Bogdan <herecomesthefog@gmail.com>
2025-06-17 16:29:33 +0500

    separate view from controller

--- src/js/solutionForm.js
+++ src/js/solutionForm.js
@@ -43,7 +43,7 @@ SolutionEditorView.prototype.createCustomViewStyle = function() {
     });
 }
 
-function SolutionFormView(parent) {
+export function SolutionFormView(parent) {
     const root = UIComponentFactory.createTaskForm();
 
     this.editor = new SolutionEditorView(root);
@@ -57,9 +57,8 @@ function SolutionFormView(parent) {
 SolutionFormView.prototype = Object.create(ComponentView.prototype);
 SolutionFormView.prototype.constructor = SolutionFormView;
 
-export function SolutionFormController(parent) {
-    this.form = new SolutionFormView(parent);
-    ComponentController.call(this, this.form);
+export function SolutionFormController(view) {
+    ComponentController.call(this, view);
 }
 SolutionFormController.prototype = Object.create(ComponentController.prototype);
 SolutionFormController.prototype.constructor = SolutionFormController;
@@ -70,26 +69,26 @@ SolutionFormController.prototype.init = function(data = null) {
 
     if (data) {
         const currentTaskObj = JSON.parse(data);
-        const value = this.form.editor.state.update({
+        const value = this.view.editor.state.update({
             changes: {from: 0, insert: currentTaskObj.code}
         });
-        this.form.editor.dispatch(value);
+        this.view.editor.dispatch(value);
     }
 }
 SolutionFormController.prototype.disableSaveButton = function() {
-    this.form.editor.dispatch({
+    this.view.editor.dispatch({
         effects: StateEffect.appendConfig.of(
             EditorView.updateListener.of(upd => {
                 if (upd.docChanged) {
-                    this.form.saveButton.disabled = upd.view.state.doc.length <= 1;
+                    this.view.saveButton.disabled = upd.view.state.doc.length <= 1;
                 }
             })
         )
     });
 }
 SolutionFormController.prototype.execute = function() {
-    this.form.executeButton.addEventListener('click', () => {
-        const code = this.form.editor.state.doc.toString();
+    this.view.executeButton.addEventListener('click', () => {
+        const code = this.view.editor.state.doc.toString();
 
         if (code.length <= 1) return;
         
@@ -102,8 +101,8 @@ SolutionFormController.prototype.execute = function() {
     });
 }
 SolutionFormController.prototype.onSave = function(cb) {
-    this.form.saveButton.addEventListener('click', () => {
-        const data = this.form.editor.state.doc.toString();
+    this.view.saveButton.addEventListener('click', () => {
+        const data = this.view.editor.state.doc.toString();
         cb(data);
     });
 }
