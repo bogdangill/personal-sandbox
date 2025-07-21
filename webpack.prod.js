77e28d7a17b77dc5b78d18b26bda7f303a1712c4
Bogdan <herecomesthefog@gmail.com>
2025-07-21 19:33:17 +0500

    add publicPath for deploy

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,6 +18,7 @@ module.exports = merge(common, {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'build'),
         clean: true,
+        publicPath: 'auto'
     },
     plugins: [
         new MiniCssExtractPlugin({
